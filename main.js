c7dd28331bda2a864fbef6c94fbb814c55f7827f
RRocha21 <renatorocha21@ua.pt>
2022-12-28 23:19:11 +0000

    Start

---
+++ main.js
@@ -0,0 +1,135 @@
+const {MongoClient} = require('mongodb');
+const Express = require('express');
+const app = Express();
+const http = require('http').Server(app);
+const Cors = require('cors');
+const { response } = require('express');
+const { emit } = require('process');
+
+const io = require("socket.io")(port = 3002, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        }
+    }
+);
+
+app.use(Cors());
+uri = 'mongodb+srv://RRocha21:benfica10@g2.tehz06h.mongodb.net/?retryWrites=true&w=majority';
+const mongoClient = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+app.get("/staticoverlays", async (req, res) => {
+    try {
+        let result = await mongoClient.db("test").collection('staticoverlays').find({}).toArray();
+        res.send(result);
+    } catch (error) {
+        response.status(500).send({error: error.message});
+    }
+});
+
+app.get("/streamers", async (req, res) => {
+    try {
+        let result = await mongoClient.db("test").collection('streamers').find({}).toArray();
+        res.send(result);
+    } catch (error) {
+        response.status(500).send({error: error.message});
+    }
+});
+
+app.get("/campaigns", async (req, res) => {
+    try {
+        let result = await mongoClient.db("test").collection('campaigns').find({}).toArray();
+        res.send(result);
+    } catch (error) {
+        response.status(500).send({error: error.message});
+    }
+});
+
+app.get("/groups", async (req, res) => {
+    try {
+        let result = await mongoClient.db("test").collection('groups').find({}).toArray();
+        res.send(result);
+    } catch (error) {
+        response.status(500).send({error: error.message});
+    }
+});
+
+app.get("/staticarts", async (req, res) => {
+    try {
+        let result = await mongoClient.db("test").collection('staticarts').find({}).toArray();
+        res.send(result);
+    } catch (error) {
+        response.status(500).send({error: error.message});
+    }
+});
+
+app.get("/triggers", async (req, res) => {
+    try {
+        let result = await mongoClient.db("test").collection('triggers').find({}).toArray();
+        res.send(result);
+    } catch (error) {
+        response.status(500).send({error: error.message});
+    }
+});
+
+app.get("/sponsors", async (req, res) => {
+    try {
+        let result = await mongoClient.db("test").collection('sponsors').find({}).toArray();
+        res.send(result);
+    } catch (error) {
+        response.status(500).send({error: error.message});
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.emit('message', 'a user connected');
+});
+
+
+
+StaticOverlay = mongoClient.db("test").collection('staticoverlays');
+changeOverlay = StaticOverlay.watch();
+changeOverlay.on("change", async (change) => {
+        let result = await mongoClient.db("test").collection('staticoverlays').find({}).toArray();
+        let result2 = await mongoClient.db("test").collection('staticarts').find({}).toArray();
+        console.log(result2)
+        io.emit("changeOverlay", {result, result2});
+});
+
+Streamer = mongoClient.db("test").collection('streamers');
+changeStreamer = Streamer.watch();
+changeStreamer.on("change", async (change) => {
+        console.log("enviado");
+        let result = await mongoClient.db("test").collection('streamers').find({}).toArray();
+        let result2 = await mongoClient.db("test").collection('groups').find({}).toArray();
+        let result3 = await mongoClient.db("test").collection('sponsors').find({}).toArray();
+        io.emit("changeStreamer", {result, result2, result3});
+});
+
+StaticArt = mongoClient.db("test").collection('staticarts');
+changeStaticArt = StaticArt.watch();
+changeStaticArt.on("change", async (change) => {
+        console.log("enviado");
+        let result = await mongoClient.db("test").collection('staticarts').find({}).toArray();
+        io.emit("changeStaticArt", result);
+});
+
+Group = mongoClient.db("test").collection('groups');
+changeGroup = Group.watch();
+changeGroup.on("change", async (change) => {
+        console.log("enviado");
+        let result = await mongoClient.db("test").collection('groups').find({}).toArray();
+        io.emit("changeGroup", result);
+});
+
+app.listen(3001, async () => {
+    try {
+        await mongoClient.connect();
+
+        console.log('listening on *:3001');
+    } catch (error) {
+        console.log(error);
+    }
+});
+
